8177e5953f8825b225ed2e7e266b79bad386c537
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-17 20:43:26 -0600

    Route for logging out user

--- src/routers/user.js
+++ src/routers/user.js
@@ -25,7 +25,31 @@ router.post('/users/login', async (req, res) => {
 	}
 });
 
-router.get('/users', auth, async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
+	try {
+		req.user.tokens = req.user.tokens.filter(token => {
+			return token.token !== req.token;
+		});
+		await req.user.save();
+
+		res.send();
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+	try {
+		req.user.tokens = [];
+		await req.user.save();
+
+		res.send();
+	} catch (e) {
+		res.status(500).send();
+	}
+})
+
+router.get('/users', async (req, res) => {
 	try {
 		const users = await User.find({});
 		res.send(users);
@@ -35,8 +59,8 @@ router.get('/users', auth, async (req, res) => {
 });
 
 router.get('/users/me', auth, async (req, res) => {
-	res.send(req.user)
-})
+	res.send(req.user);
+});
 
 router.get('/users/:id', async (req, res) => {
 	const _id = req.params.id;
