aaa841297e237b348911d2fb38da48d34e256729
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-17 20:12:53 -0600

    Authenticate user when logged in

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/tasks');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+// 	if (req.method === 'GET') {
+// 		res.send('GET requests are disabled');
+// 	} else {
+// 		next();
+// 	}
+// });
+
+// app.use((req, res, next) => {
+// 	res.status(503).send({ message: 'Site is currently down. Check back later.' });
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -14,8 +26,6 @@ app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
 
-const myFunction = async () => {
+const myFunction = async () => {};
 
-}
-
-myFunction()
+myFunction();
